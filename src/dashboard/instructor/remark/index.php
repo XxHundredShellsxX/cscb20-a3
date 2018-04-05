6df9d8ae9e4e9dea9a233aa0ee27d6aa0fd04109
Rikin Katyal <me@sirvar.com>
2018-04-05 14:08:22 -0400

    instructor remark

---
+++ src/dashboard/instructor/remark/index.php
@@ -0,0 +1,135 @@
+<?php
+  include("../../../auth/config.php");
+  session_start();
+  if (!isset($_SESSION['token'])){
+    header("Location:../../../auth/login");
+  }
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $assessment = $_GET['assessment'];
+    $current_mark = $_SESSION[$assessment];
+    date_default_timezone_set('EST');
+    $sql = "insert into Remarks values ('".$_SESSION['utorid']."', '".date("Y-m-d H:i:s")."','0', null, $current_mark, null, '$assessment', \"".$_POST['remarkBody']."\", null)";
+    if (mysqli_query($db, $sql)) {
+      alert("Remark request successfully submitted");
+    } else {
+      alert("There was a problem submitting the remark request");
+    }
+  }
+  function nl2p($text) {
+    return '<p>'.str_replace(array("\r\n", "\r", "\n"), '</p><p>', $text).'</p>';
+  }
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  
+    <script src="../../../js/index.js"></script>
+  
+    <link rel="stylesheet" type="text/css" href="../../../css/feather.css">
+    <link rel="stylesheet" type="text/css" href="../../../css/index.css">
+  
+    <title>Dashboard | CSCB20</title>
+
+    <script>
+      function change_mark(assessment) {
+        window.location = 'index.php?assessment=' + assessment.value;
+      }
+    </script>
+</head>
+<body>
+  <div id="container">
+      <div id="sidebar">
+          <div id="menu" class="round-button" onclick="toggleMenu()"><i class="feather icon-menu"></i></div>
+          <h1>cscb20</h1>
+          <h4>introduction to databases & web applications</h4>
+          <nav>
+            <a href="../../../">
+              <div class="nav-item">
+                <h2><i class="feather icon-home"></i>home</h2>
+              </div>
+            </a>
+            <a href="../">
+              <div class="nav-item">
+                <h2><i class="feather icon-monitor"></i>dashboard</h2>
+              </div>
+            </a>
+            <a href="./">
+              <div class="nav-item active">
+                <h2><i class="feather icon-hash"></i>marks</h2>
+              </div>
+            </a>
+            <a href="../feedback/">
+              <div class="nav-item">
+                <h2><i class="feather icon-clipboard"></i>feedback </h2>
+              </div>
+            </a>
+            <a href="../../../auth/logout/">
+              <div class="nav-item">
+                <h2><i class="feather icon-log-out"></i>logout</h2>
+              </div>
+            </a>
+          </nav>
+        </div>
+    <div id="content">
+      <h1>Remark Request</h1>
+      <h2>View current and past remark requests</h2>
+      <?php
+        $sql = "select * from Remarks where approved = 0";
+        $result = mysqli_query($db, $sql);
+        while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+          echo "<div class='card remark'>";
+          echo "<div class='assesment-grade'>";
+          echo "<h2>remark for: ".$row['assessment']."</h2>";
+          echo "<h2>original mark: ".$row['originalGrade']."%</h2>";
+          echo "<h2>update mark: </h2><input type='number' value='".$row['originalGrade']."' />";
+          if ($row['approved'] == 0) {
+            $status = "pending";
+            $feedback = "";
+          } else {
+            $status = "completed";
+            $feedback = "<h2>Remarked by ".$row['approvedBy']."</h2>".nl2p($row['remarkFeedback']);
+            echo "<h2>updated mark: ".$row['updatedGrade']."%</h2>";
+          }
+          echo "<h2>status: ".$status."</h2>";
+          echo "</div>";
+          echo nl2p($row['remarkBody']);
+          echo $feedback;
+          echo "<textarea placeholder='Enter a response for the request.'></textarea>";
+          echo "<button>Resolve</button>";
+          echo "</div>";
+        }
+      ?>
+      <h2>Previous Remark Requests</h2>
+      <?php
+        $sql = "select * from Remarks where approved = 1";
+        $result = mysqli_query($db, $sql);
+        while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+          echo "<div class='card remark'>";
+          echo "<div class='assesment-grade'>";
+          echo "<h2>remark for: ".$row['assessment']."</h2>";
+          echo "<h2>original mark: ".$row['originalGrade']."%</h2>";
+          if ($row['approved'] == 0) {
+            $status = "pending";
+            $feedback = "";
+          } else {
+            $status = "completed";
+            $feedback = "<h2>Remarked by ".$row['approvedBy']."</h2>".nl2p($row['remarkFeedback']);
+            echo "<h2>updated mark: ".$row['updatedGrade']."%</h2>";
+          }
+          echo "<h2>status: ".$status."</h2>";
+          echo "</div>";
+          echo nl2p($row['remarkBody']);
+          echo $feedback;
+          echo "</div>";
+        }
+      ?>
+      <footer>
+        <p><b>Made with <i class="feather icon-heart"></i> by Rikin Katyal & Sajid Rahman</b></p>
+        <p><a href="https://www.utoronto.ca/" target="_">University of Toronto</a> | <a href="http://web.cs.toronto.edu/" target="_">U of T Department of Computer Science</a> | <a href="http://www.utsc.utoronto.ca/home/" target="_">UTSC</a> | <a href="https://www.utsc.utoronto.ca/cms/computer-science-mathematics-statistics" target="_">UTSC CMS</a> | <a href="http://www.utsc.utoronto.ca/labs/"> UTSC Labs</a></p>
+      </footer>
+    </div>
+  </div>
+</body>
+</html>
